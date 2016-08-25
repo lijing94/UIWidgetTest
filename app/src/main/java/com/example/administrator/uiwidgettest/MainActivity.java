5354f909774871474180ca8d48961fbcb35cf43b
lijing <lijing94@outlook.com>
2016-08-25 19:47:41 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml

--- app/src/main/java/com/example/administrator/uiwidgettest/MainActivity.java
+++ app/src/main/java/com/example/administrator/uiwidgettest/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //在此处添加逻辑
+                //逻辑1
+
             }
         });
     }
