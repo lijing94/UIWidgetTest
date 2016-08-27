91721adb06d9b11d2bd0dde61346ca8b9d85188f
lijing94 <lijing94@outlook.com>
2016-08-27 12:08:42 +0800

    111

--- app/src/main/java/com/example/administrator/uiwidgettest/MainActivity.java
+++ app/src/main/java/com/example/administrator/uiwidgettest/MainActivity.java
@@ -4,10 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button button;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,13 +18,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = (Button)  findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //在此处添加逻辑
-                //逻辑1
+        button.setOnClickListener(this);        //要加implements View.OnClickListener才能用this
 
-            }
-        });
+        editText = (EditText) findViewById(R.id.edit_text);
+    }
+    @Override
+    public void onClick(View v) {
+        //在此处添加逻辑
+        //从editText处得到内容
+        switch(v.getId()){
+            case R.id.button :
+                String inputText = editText.getText().toString();
+                Toast.makeText(MainActivity.this, inputText, Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+
+
     }
 }
